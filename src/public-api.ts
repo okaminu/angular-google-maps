875297f74ea859fda52a123eb75cd46b2c529088
Andrius Degutis <andriusdgt@gmail.com>
2019-05-14 21:07:49 +0300

    BA-502: added missing export

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of angular-google-maps
  */
 
+export * from './google-maps-wrapper.service'
 export * from './angular-google-maps.service'
 export * from './angular-google-maps.component'
 export * from './angular-google-maps.module'
