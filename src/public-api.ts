7493367cc950bcd57e03bd99c990a3bfb3d88761
Andrius Degutis <andriusdgt@gmail.com>
2019-05-14 23:23:11 +0300

    BA-502: location object made public

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,7 +2,8 @@
  * Public API Surface of angular-google-maps
  */
 
-export * from './google-maps-singleton.service'
-export * from './angular-google-maps.service'
 export * from './angular-google-maps.component'
 export * from './angular-google-maps.module'
+export * from './angular-google-maps.service'
+export * from './google-maps-singleton.service'
+export * from './location'
