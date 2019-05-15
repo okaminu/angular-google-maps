f32f1b261339ff0a143dc54ef2902ad2be27aa72
Andrius Degutis <andriusdgt@gmail.com>
2019-05-15 17:34:16 +0300

    BA-502: added two more handlers

--- src/angular-google-maps-listener.service.ts
+++ src/angular-google-maps-listener.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core'
 import { EventPublisher } from '@boldadmin/event-publisher'
+import { AngularGoogleMapsService } from './angular-google-maps.service'
+import { Location } from './location'
+import Map = google.maps.Map
 import Marker = google.maps.Marker
 
 @Injectable()
 export class AngularGoogleMapsListenerService {
 
-    constructor(private eventPublisher: EventPublisher) {
+    constructor(
+        private googleMapsService: AngularGoogleMapsService,
+        private eventPublisher: EventPublisher) {
     }
 
     getLocationDeletedMarkerHandler(marker: Marker) {
@@ -15,6 +20,26 @@ export class AngularGoogleMapsListenerService {
 
             const searchBoxInput = <HTMLInputElement>document.getElementById('search-input')
             searchBoxInput.value = ''
+        }
+    }
+
+    getLocationChangedMarkerHandler() {
+        return mouseEvent => {
+            this.eventPublisher.notify('locationChanged',
+                new Location(mouseEvent.latLng.lat(), mouseEvent.latLng.lng())
+            )
+            this.googleMapsService.reverseGeocode(new Location(mouseEvent.latLng.lat(), mouseEvent.latLng.lng()))
+        }
+    }
+
+    getLocationChangedMapHandler(marker: Marker, map: Map) {
+        return mouseEvent => {
+            marker.setMap(map)
+            marker.setPosition(mouseEvent.latLng)
+            this.eventPublisher.notify('locationChanged',
+                new Location(mouseEvent.latLng.lat(), mouseEvent.latLng.lng())
+            )
+            this.googleMapsService.reverseGeocode(new Location(mouseEvent.latLng.lat(), mouseEvent.latLng.lng()))
         }
     }
 
