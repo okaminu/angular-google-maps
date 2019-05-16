c225a9599d76a92c232e6fb348a430f70724b833
Okaminu <aurimasdgt@gmail.com>
2019-05-16 17:12:38 +0300

    BA-502: broken build fix

--- src/public-api.ts
+++ src/public-api.ts
@@ -4,6 +4,6 @@
 
 export * from './angular-google-maps.component'
 export * from './angular-google-maps.module'
-export * from './angular-google-maps.service'
-export * from './google-maps-singleton.service'
+export * from './service/angular-google-maps.service'
+export * from './service/google-maps-singleton.service'
 export * from './location'
