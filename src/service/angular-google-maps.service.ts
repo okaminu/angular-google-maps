ae4f9ad25b102810739d2b5aebc6201e163d8466
Andrius Degutis <andriusdgt@gmail.com>
2019-05-18 00:14:20 +0300

    BA-502: added map building logic to service

--- src/service/angular-google-maps.service.ts
+++ src/service/angular-google-maps.service.ts
@@ -1,11 +1,52 @@
 import { Injectable } from '@angular/core'
+import { Location } from '../location'
+import { AngularGoogleMapsListenerService } from './angular-google-maps-listener.service'
 import { GoogleMapsService } from './google-maps.service'
 import Map = google.maps.Map
+import MapOptions = google.maps.MapOptions
+import Marker = google.maps.Marker
+import MarkerOptions = google.maps.MarkerOptions
 
 @Injectable()
 export class AngularGoogleMapsService {
 
-    constructor(private googleMaps: GoogleMapsService) {
+    private map: Map
+    private marker: Marker
+
+    constructor(private googleMaps: GoogleMapsService,
+                private googleMapsListeners: AngularGoogleMapsListenerService) {
+    }
+
+    createMap(mapOptions: MapOptions, focusLocation: Location) {
+        mapOptions.center = {
+            lat: focusLocation.latitude,
+            lng: focusLocation.longitude
+        }
+        this.map = this.googleMaps.createMap(mapOptions)
+        return this
+    }
+
+    addMarker(markerOptions: MarkerOptions, areMarkerLocationsProvided: boolean) {
+        this.bindMarkerToMapsIfLocationIsProvided(markerOptions, areMarkerLocationsProvided)
+        this.marker = this.googleMaps.createMarker(markerOptions)
+        this.addMarkerListeners()
+        return this
+    }
+
+    addSearchBox() {
+        const searchBoxInput = <HTMLInputElement>document.getElementById('search-input')
+        const searchBox = this.googleMaps.createSearchBox(searchBoxInput)
+
+        this.map.controls[this.googleMaps.getGoogleMaps().ControlPosition.TOP_LEFT].push(searchBoxInput)
+
+        searchBox.addListener('places_changed',
+            this.googleMapsListeners.getLocationChangedSearchBoxMapMarkerHandler(searchBox, this.map, this.marker))
+
+        return this
+    }
+
+    build() {
+        return this.map
     }
 
     createSearchBox(map: Map) {
@@ -23,4 +64,15 @@ export class AngularGoogleMapsService {
         return Promise.resolve(resizeControl)
     }
 
+    private addMarkerListeners() {
+        this.marker.addListener('dragend', this.googleMapsListeners.getLocationChangedHandler())
+        this.marker.addListener('dblclick', this.googleMapsListeners.getLocationDeletedMarkerHandler(this.marker))
+        this.map.addListener('click', this.googleMapsListeners.getBindMarkerToMapHandler(this.marker, this.map))
+    }
+
+    private bindMarkerToMapsIfLocationIsProvided(markerOptions: MarkerOptions, isLocationProvided: boolean) {
+        markerOptions.position = this.map.getCenter()
+        if (isLocationProvided)
+            markerOptions.map = this.map
+    }
 }
--- src/test/angular-google-maps.service.spec.ts
+++ src/test/angular-google-maps.service.spec.ts
@@ -1,6 +1,9 @@
 import { fakeAsync, TestBed } from '@angular/core/testing'
+import { Location } from '../location'
+import { AngularGoogleMapsListenerService } from '../service/angular-google-maps-listener.service'
 import { AngularGoogleMapsService } from '../service/angular-google-maps.service'
 import { GoogleMapsService } from '../service/google-maps.service'
+import Marker = google.maps.Marker
 import createSpy = jasmine.createSpy
 import createSpyObj = jasmine.createSpyObj
 import SpyObj = jasmine.SpyObj
@@ -9,6 +12,10 @@ describe('AngularGoogleMapsService', () => {
 
     let googleMaps: SpyObj<GoogleMapsService>
     let service: AngularGoogleMapsService
+    let listenerServiceSpy: SpyObj<AngularGoogleMapsListenerService>
+
+    const position = {lat: 10, lng: 15}
+    const focusLocation = new Location(10, 15)
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -16,65 +23,26 @@ describe('AngularGoogleMapsService', () => {
                 AngularGoogleMapsService,
                 {
                     provide: GoogleMapsService,
-                    useValue: createSpyObj('GoogleMapsService', ['getGoogleMaps', 'createSearchBox'])
+                    useValue: createSpyObj('GoogleMapsService',
+                        ['getGoogleMaps', 'createMap', 'createMarker', 'createSearchBox'])
+                },
+                {
+                    provide: AngularGoogleMapsListenerService,
+                    useValue: createSpyObj('AngularGoogleMapsListenerService',
+                        ['getLocationChangedHandler', 'getBindMarkerToMapHandler',
+                            'getLocationChangedSearchBoxMapMarkerHandler', 'getLocationDeletedMarkerHandler'])
                 }
             ]
         })
         googleMaps = TestBed.get(GoogleMapsService)
+        listenerServiceSpy = TestBed.get(AngularGoogleMapsListenerService)
+
         service = TestBed.get(AngularGoogleMapsService)
     })
 
     afterEach(() =>
         document.getElementById = createSpy('document').and.callThrough()
     )
-
-    describe('SearchBox', () => {
-
-        const position = {lat: 10, lng: 10}
-        let elementSpy: SpyObj<HTMLElement>
-        let controlSpy: SpyObj<google.maps.MVCArray<Node>[]>
-        let mapSpy: SpyObj<any>
-        let markerSpy: SpyObj<google.maps.Marker>
-        let searchBoxSpy: SpyObj<google.maps.places.SearchBox>
-
-        beforeEach(() => {
-            document.getElementById = createSpy('document').and.callFake(id => {
-                if (id === 'search-input')
-                    return elementSpy
-                else throw Error()
-            })
-            elementSpy = createSpyObj('HTMLElement', [''])
-            controlSpy = createSpyObj('google.maps.Map.controls', ['push'])
-            mapSpy = createSpyObj('google.maps.Map', ['controls', 'panTo', 'setZoom'])
-            markerSpy = createSpyObj('google.maps.Marker', ['setPosition', 'setMap'])
-            searchBoxSpy = createSpyObj('google.maps.places.SearchBox', ['addListener', 'getPlaces'])
-
-            mapSpy.controls = {
-                'somePosition': controlSpy
-            }
-            searchBoxSpy.getPlaces.and.returnValue([{
-                geometry: {
-                    location: {
-                        lat: () => position.lat,
-                        lng: () => position.lng
-                    }
-                }
-            }] as any[])
-            googleMaps.createSearchBox.and.returnValue(searchBoxSpy)
-            googleMaps.getGoogleMaps.and.returnValue({
-                ControlPosition: {
-                    TOP_LEFT: 'somePosition'
-                }
-            })
-        })
-
-        it('is added to map', fakeAsync(() => {
-            const searchBox = service.createSearchBox(mapSpy)
-
-            expect(searchBox).toBe(searchBoxSpy)
-            expect(controlSpy.push).toHaveBeenCalledWith(elementSpy)
-        }))
-    })
 
     describe('Custom expand control', () => {
         let elementSpy: SpyObj<HTMLElement>
@@ -109,6 +77,204 @@ describe('AngularGoogleMapsService', () => {
                     expect(controlSpy.push).toHaveBeenCalledWith(elementSpy)
                 })
         }))
+
+    })
+
+    describe('Building Google Maps', () => {
+
+        let mapSpy: SpyObj<any>
+        let markerSpy: SpyObj<Marker>
+        let mapOptionsSpy: SpyObj<any>
+        let markerOptionsSpy: SpyObj<any>
+
+        beforeEach(() => {
+            mapSpy = createSpyObj('google.maps.Map', ['getCenter', 'addListener'])
+            markerSpy = createSpyObj('google.maps.Marker', ['addListener'])
+            mapOptionsSpy = createSpyObj('google.maps.MapOptions', [''])
+            markerOptionsSpy = createSpyObj('google.maps.MarkerOptions', [''])
+
+            googleMaps.createMap.and.returnValue(mapSpy)
+            googleMaps.createMarker.and.returnValue(markerSpy)
+        })
+
+        describe('On map building', () => {
+
+            it('builds a map', () => {
+                expect(service
+                    .createMap(mapOptionsSpy, mapOptionsSpy)
+                    .build()
+                ).toBe(mapSpy)
+            })
+
+            it('map is centered by provided location', () => {
+                service
+                    .createMap(mapOptionsSpy, focusLocation)
+                    .build()
+
+                expect(mapOptionsSpy.center).toEqual({
+                    lat: focusLocation.latitude,
+                    lng: focusLocation.longitude
+                })
+                expect(googleMaps.createMap).toHaveBeenCalledWith(mapOptionsSpy)
+            })
+
+        })
+
+        describe('On marker building', () => {
+
+            let handlerDummy: SpyObj<() => void>
+
+            beforeEach(() => {
+                handlerDummy = createSpyObj('ListenerHandler', [''])
+            })
+
+            it('adds a marker', () => {
+                service
+                    .createMap(mapOptionsSpy, focusLocation)
+                    .addMarker(markerOptionsSpy, true)
+                    .build()
+
+                expect(googleMaps.createMarker).toHaveBeenCalled()
+            })
+
+            it('adds a marker aligned with map position', () => {
+                mapSpy.getCenter.and.returnValue(position)
+
+                service
+                    .createMap(mapOptionsSpy, focusLocation)
+                    .addMarker(markerOptionsSpy, true)
+                    .build()
+
+                expect(markerOptionsSpy.position).toEqual(position)
+            })
+
+            it('marker is bound to map if marker location are provided', () => {
+                service
+                    .createMap(mapOptionsSpy, focusLocation)
+                    .addMarker(markerOptionsSpy, true)
+                    .build()
+
+                expect(markerOptionsSpy.map).toEqual(mapSpy)
+            })
+
+            it('marker is not bound to map if location is not provided', () => {
+                service
+                    .createMap(mapOptionsSpy, focusLocation)
+                    .addMarker(markerOptionsSpy, false)
+                    .build()
+
+                expect(markerOptionsSpy.map).toBeUndefined()
+            })
+
+            it('adds location changed marker listener', () => {
+                listenerServiceSpy.getLocationChangedHandler.and.returnValue(handlerDummy)
+
+                service
+                    .createMap(mapOptionsSpy, focusLocation)
+                    .addMarker(markerOptionsSpy, true)
+                    .build()
+
+                expect(markerSpy.addListener).toHaveBeenCalledWith('dragend', handlerDummy)
+                expect(listenerServiceSpy.getLocationChangedHandler).toHaveBeenCalled()
+            })
+
+            it('adds location deleted marker listener', () => {
+                listenerServiceSpy.getLocationDeletedMarkerHandler.and.returnValue(handlerDummy)
+
+                service
+                    .createMap(mapOptionsSpy, focusLocation)
+                    .addMarker(markerOptionsSpy, true)
+                    .build()
+
+                expect(markerSpy.addListener).toHaveBeenCalledWith('dblclick', handlerDummy)
+                expect(listenerServiceSpy.getLocationDeletedMarkerHandler).toHaveBeenCalledWith(markerSpy)
+            })
+
+            it('binds map click to marker position update', () => {
+                listenerServiceSpy.getBindMarkerToMapHandler.and.returnValue(handlerDummy)
+
+                service
+                    .createMap(mapOptionsSpy, focusLocation)
+                    .addMarker(markerOptionsSpy, true)
+                    .build()
+
+                expect(mapSpy.addListener).toHaveBeenCalledWith('click', handlerDummy)
+                expect(listenerServiceSpy.getBindMarkerToMapHandler).toHaveBeenCalledWith(markerSpy, mapSpy)
+            })
+
+        })
+
+        describe('On search box creation', () => {
+
+            let elementSpy: SpyObj<HTMLElement>
+            let controlSpy: SpyObj<google.maps.MVCArray<Node>[]>
+            let searchBoxSpy: SpyObj<google.maps.places.SearchBox>
+
+            beforeEach(() => {
+                document.getElementById = createSpy('document').and.callFake(id => {
+                    if (id === 'search-input')
+                        return elementSpy
+                    else throw Error()
+                })
+                elementSpy = createSpyObj('HTMLElement', [''])
+                controlSpy = createSpyObj('google.maps.Map.controls', ['push'])
+                searchBoxSpy = createSpyObj('google.maps.places.SearchBox', ['addListener', 'getPlaces'])
+
+                mapSpy.controls = {
+                    'somePosition': controlSpy
+                }
+                searchBoxSpy.getPlaces.and.returnValue([{
+                    geometry: {
+                        location: {
+                            lat: () => position.lat,
+                            lng: () => position.lng
+                        }
+                    }
+                }] as any[])
+                googleMaps.createSearchBox.and.returnValue(searchBoxSpy)
+                googleMaps.getGoogleMaps.and.returnValue({
+                    ControlPosition: {
+                        TOP_LEFT: 'somePosition'
+                    }
+                })
+            })
+
+            it('adds search box', fakeAsync(() => {
+                service
+                    .createMap(mapOptionsSpy, focusLocation)
+                    .addMarker(markerOptionsSpy, true)
+                    .addSearchBox()
+                    .build()
+
+                expect(googleMaps.createSearchBox).toHaveBeenCalledWith(elementSpy)
+            }))
+
+            it('adds search box with configured location', fakeAsync(() => {
+                service
+                    .createMap(mapOptionsSpy, focusLocation)
+                    .addMarker(markerOptionsSpy, true)
+                    .addSearchBox()
+                    .build()
+
+                expect(controlSpy.push).toHaveBeenCalledWith(elementSpy)
+            }))
+
+            it('adds listener for a search box', () => {
+                const handlerDummy: SpyObj<() => void> = createSpyObj('ListenerHandler', [''])
+                listenerServiceSpy.getLocationChangedSearchBoxMapMarkerHandler.and.returnValue(handlerDummy)
+
+                service
+                    .createMap(mapOptionsSpy, focusLocation)
+                    .addMarker(markerOptionsSpy, true)
+                    .addSearchBox()
+                    .build()
+
+                expect(searchBoxSpy.addListener).toHaveBeenCalledWith('places_changed', handlerDummy)
+                expect(listenerServiceSpy.getLocationChangedSearchBoxMapMarkerHandler)
+                    .toHaveBeenCalledWith(searchBoxSpy, mapSpy, markerSpy)
+            })
+
+        })
 
     })
 
